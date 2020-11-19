6b5ef0f3f2a486c14c6b4d8b8b7d928f003c7aa8
nguyenxuanchien92 <nguyenxuanchien92@gmail.com>
2020-11-19 16:23:26 +0700

    bài tập thực hành về list trong collection

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println("element 4: " + listInteger.get(4));
+        System.out.println("element 1: " + listInteger.get(1));
+        System.out.println("element 2: " + listInteger.get(2));
+
+//        listInteger.get(6);
+//        System.out.println("element 6: " + listInteger.get(6));
+
+//        listInteger.get(-1);
+//        System.out.println("element -1: " + listInteger.get(-1));
+
+    }
+}
